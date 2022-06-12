064ca65196dd5453f19d771cfe3229a937635e80
Dinçer Ülker <dincerulker@gmail.com>
2022-06-12 17:30:38 +0300

    slick carousel added
    carousel added and some fixing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-const bootstrap = require('bootstrap')
+
+$(document).ready(function(){
+    $('.slider-slick').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      fade: true,
+      autoplaySpeed: 2000,
+      cssEase: 'linear'
+    });
+  });
